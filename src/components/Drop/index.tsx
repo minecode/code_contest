bd25e94dc2a8dcf3c733b3e73ffe569a8a7adbb4
cfchenr <c.henriques@ua.pt>
2020-08-06 21:00:01 +0100

    Update workflow

--- src/components/Drop/index.tsx
+++ src/components/Drop/index.tsx
@@ -26,7 +26,7 @@ const Drop: React.FC = () => {
 
     const config = {
         headers: {
-            Authorization: `Basic ${process.env.REACT_APP_APIKEY}`,
+            Authorization: `token ${process.env.REACT_APP_APIKEY}`,
             Accept: 'application/vnd.github.antiope-preview+json'
         }
     }
