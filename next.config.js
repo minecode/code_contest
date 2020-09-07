cf113b37ca7a7a839dfed335a4e0e66c01a56072
cfchenr <c.henriques@ua.pt>
2020-09-07 13:25:22 +0100

    Fix workflow and deploy

--- next.config.js
+++ next.config.js
@@ -1,43 +1,10 @@
 const withImages = require('next-images')
 
-const debug = process.env.NODE_ENV !== "production";
-
 module.exports = withImages({
-	esModule: true,
+	esModule: true
 })
 
-
 module.exports = {
-	exportPathMap: function () {
-		return {
-			"/": {
-				page: "/"
-			},
-		}
-	},
-	//assetPrefix: '',
-	assetPrefix: !debug ? '/code_contest_frontend/' : '',
-	webpack: (config, {
-		dev
-	}) => {
-		// Perform customizations to webpack config
-		// console.log('webpack');
-		// console.log(config.module.rules, dev);
-		config.module.rules = config.module.rules.map(rule => {
-			if (rule.loader === 'babel-loader') {
-				rule.options.cacheDirectory = false
-			}
-			return rule
-		})
-		// Important: return the modified config
-		return config
-	}
-	/*,
-		webpackDevMiddleware: (config) => {
-		  // Perform customizations to webpack dev middleware config
-		  // console.log('webpackDevMiddleware');
-		  // console.log(config);
-		  // Important: return the modified config
-		  return config
-		}, */
+	basePath: process.env.NEXT_PUBLIC_BASE_PATH,
+	assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH
 }
