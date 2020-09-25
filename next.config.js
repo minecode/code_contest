55925a4241612e8cf8269d760565c176b4a8cccb
cfchenr <c.henriques@ua.pt>
2020-09-25 11:14:25 +0100

    Remove config in next.config.js

--- next.config.js
+++ next.config.js
@@ -8,13 +8,3 @@ module.exports = {
 	basePath: process.env.NEXT_PUBLIC_BASE_PATH,
 	assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH
 }
-
-module.exports = {
-	webpack: function (config) {
-		config.module.rules.push({
-			test: /\.md$/,
-			use: 'raw-loader',
-		})
-		return config
-	},
-}
